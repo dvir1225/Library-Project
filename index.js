33fb3f26a4daddd36067e20ba9997257c489027e
Dvir <dvir1225@gmail.com>
2022-09-05 15:25:04 +0300

    improve makeCards function, add basic css

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 const mobyDick = 
-new Book("Moby Dick", "Herman Melville", 427, "unread" )
+new Book("Moby Dick", "Herman Melville", 427, "Unread" )
 const catcher = 
-new Book("The catcher in the Rye", "J.D. Salinger", 208, "read" )
+new Book("The catcher in the Rye", "J.D. Salinger", 208, "Read" )
 const narnia = 
-new Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 427, "unread" )
+new Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 427, "Unread" )
 const harryPotter = 
-new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 309, "read" )
+new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 309, "Read" )
 const lordOfRings = 
-new Book("The Lord of the Rings", "J.R.R. Tolkien", 1216, "unread" )
+new Book("The Lord of the Rings", "J.R.R. Tolkien", 1216, "Unread" )
 const songOfIceFire =
-new Book("A Song of Ice and Fire", "George R.R.Martin", 720, "read" )
+new Book("A Song of Ice and Fire", "George R.R.Martin", 720, "Read" )
 
 function addBookToLibrary(book){
     myLibrary.push(book);
@@ -30,17 +30,39 @@ addBookToLibrary(harryPotter);
 addBookToLibrary(lordOfRings);
 addBookToLibrary(songOfIceFire);
 
+let cards;
+
 function makeCards(myLibrary){
-    let cards = document.querySelector('.cards');
+    cards = document.querySelector('.cards');
     for (let i = 0; i < myLibrary.length; i++){
         let card = document.createElement("div");
         card.setAttribute('class', 'card');
         card.setAttribute('id', `${i}`);
-        card.innerText =
-        `Title: ${myLibrary[i].title}
-        Author: ${myLibrary[i].author}
-        Pages: ${myLibrary[i].pages}
-        Read: ${myLibrary[i].read}`;
+
+        let title = document.createElement('span');
+        title.setAttribute('class', 'title')
+
+        let author = document.createElement('span');
+        author.setAttribute('class', 'author');
+
+        let pages = document.createElement('span');
+        pages.setAttribute('class', 'pages');
+
+        let read = document.createElement('span');
+        read.setAttribute('class', 'read');
+
+        card.appendChild(title)
+        card.appendChild(author)
+        card.appendChild(pages)
+        card.appendChild(read)
+
+        title.innerText = `${myLibrary[i].title}`
+        author.innerText = `${myLibrary[i].author}`
+        pages.innerText = `${myLibrary[i].pages}`
+        read.innerText = `${myLibrary[i].read}`
+        
+        ;
+        
         cards.appendChild(card);
     }
 }
