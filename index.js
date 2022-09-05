86af9b40c70ef6e2c6f5f4564b06b3cc5f662add
Dvir <dvir1225@gmail.com>
2022-09-05 18:22:34 +0300

    add remove book and change read status buttons to book cards

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const songOfIceFire =
 new Book("A Song of Ice and Fire", "George R.R.Martin", 720, "Read" )
 
 function addBookToLibrary(book){
-    myLibrary.push(book);
+   myLibrary.push(book);
 }
 
 addBookToLibrary(mobyDick);
@@ -31,14 +31,14 @@ addBookToLibrary(lordOfRings);
 addBookToLibrary(songOfIceFire);
 
 let cards;
+let removeBtn;
 
 function makeCards(myLibrary){
     cards = document.querySelector('.cards');
     for (let i = 0; i < myLibrary.length; i++){
         let card = document.createElement("div");
         card.setAttribute('class', 'card');
-        card.setAttribute('id', `${i}`);
-
+        card.setAttribute('data', `${i}`);
         let title = document.createElement('span');
         title.setAttribute('class', 'title')
 
@@ -56,13 +56,53 @@ function makeCards(myLibrary){
         card.appendChild(pages)
         card.appendChild(read)
 
-        title.innerText = `${myLibrary[i].title}`
-        author.innerText = `${myLibrary[i].author}`
-        pages.innerText = `${myLibrary[i].pages}`
-        read.innerText = `${myLibrary[i].read}`
-        
-        ;
+        title.innerText = `${myLibrary[i].title}`;
+        author.innerText = `${myLibrary[i].author}`;
+        pages.innerText = `${myLibrary[i].pages} pages long`;
+        read.innerText = `Status: ${myLibrary[i].read}`;
         
         cards.appendChild(card);
+
+        removeBtn = document.createElement('button');
+        removeBtn.setAttribute('id', `removeBtn ${i}`);
+        removeBtn.innerText = 'Remove book from library.';
+        card.appendChild(removeBtn);
+        removeBtn.addEventListener('click', (event) => {
+          myLibrary.splice(i, 1);
+          cards.removeChild(card);
+        })
+
+        const readBtn = document.createElement('button');
+        function getReadStatus(){
+            if(read.innerText === 'Status: Read'){
+                readBtn.innerText = 'Mark book unread';
+            }
+            if (read.innerText === 'Status: Unread') { 
+                readBtn.innerText = 'Mark book read';
+            }
+        }
+        getReadStatus()
+        
+        readBtn.addEventListener('click', () => {
+            if(readBtn.innerText === 'Mark book read'){
+                myLibrary[i].read = 'Read';
+                read.innerText = `Status: ${myLibrary[i].read}`;
+            } if (readBtn.innerText === 'Mark book unread') {
+                myLibrary[i].read = 'Unread';
+                read.innerText = `Status: ${myLibrary[i].read}`;
+            } getReadStatus()
+        })
+        card.appendChild(readBtn);
     }
 }
+
+
+
+const newBookBtn = document.querySelector('.newBookBtn');
+const newBookForm = document.querySelector('.newBookForm');
+
+newBookBtn.addEventListener('click', (event) => {
+    newBookForm.style.display = 'block';
+});
+
+makeCards(myLibrary);
